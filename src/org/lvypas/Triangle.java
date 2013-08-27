5e1bb7efaea999eaace1b9410934022e08644adc
lvypas <lyubic@gmail.com>
2013-08-27 15:54:36 +0300

    Tutorial 05 - ApplicationContext and Property Initialization.

--- src/org/lvypas/Triangle.java
+++ src/org/lvypas/Triangle.java
@@ -2,8 +2,18 @@ package org.lvypas;
 
 public class Triangle {
     
+    private String type;
+    
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     public void draw() {
-        System.out.println("Triangle drawn");
+        System.out.println(getType() + " Triangle drawn");
     }
 
 }
