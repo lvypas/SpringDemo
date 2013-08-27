7a7f67ac5bf99674641401fa0b58fe891f222977
lvypas <lyubic@gmail.com>
2013-08-27 17:36:46 +0300

    Tutorial 07 - Injecting Objects.

--- src/org/lvypas/DrawingApp.java
+++ src/org/lvypas/DrawingApp.java
@@ -4,13 +4,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
-    
+
     public static void main(String[] args) {
-        //Triangle triangle = new Triangle();
-        //BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                "spring.xml");
         Triangle triangle = (Triangle) context.getBean("triangle");
         triangle.draw();
+
     }
 
 }
