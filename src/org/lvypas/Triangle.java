ff46411b859fa26c135c929b4578aaaf06e960ac
lvypas <lyubic@gmail.com>
2013-08-28 14:40:48 +0300

    Tutorial 12 - Using ApplicationContextAware.

--- src/org/lvypas/Triangle.java
+++ src/org/lvypas/Triangle.java
@@ -2,11 +2,17 @@ package org.lvypas;
 
 import java.util.List;
 
-public class Triangle {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle implements ApplicationContextAware, BeanNameAware{
 
     private Point pointA;
     private Point pointB;
     private Point pointC;
+    private ApplicationContext context = null;
 
     
     public Point getPointA() {
@@ -45,6 +51,21 @@ public class Triangle {
         System.out.println("Point B = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
         System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
 
+    }
+
+
+    @Override
+    public void setApplicationContext(ApplicationContext arg0)
+            throws BeansException {
+        this.context = context;
+        
+    }
+
+
+    @Override
+    public void setBeanName(String beanName) {
+        System.out.println("Bean name is: " + beanName);
+        
     }
 
 }
