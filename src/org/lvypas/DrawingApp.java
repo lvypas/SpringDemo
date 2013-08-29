bc7c0157d6ad343928d20654a3ed8e6756640d2f
lvypas <lyubic@gmail.com>
2013-08-29 14:13:45 +0300

    Tutorial 16 - Writing a BeanFactoryPostProcessor.

--- src/org/lvypas/DrawingApp.java
+++ src/org/lvypas/DrawingApp.java
@@ -1,15 +1,14 @@
 package org.lvypas;
 
-import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
 
     public static void main(String[] args) {
 
-        AbstractApplicationContext context = new ClassPathXmlApplicationContext(
-                "spring.xml");
-        context.registerShutdownHook();
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                "spring.xml");        
         Triangle triangle = (Triangle) context.getBean("triangle");
         triangle.draw();
 
