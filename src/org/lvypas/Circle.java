7879bd5f96dc90042db89495a1ce2b2b948b5ede
lvypas <lyubic@gmail.com>
2013-08-29 17:07:24 +0300

    Tutorial 20 - Some JSR-250 Annotations.

--- src/org/lvypas/Circle.java
+++ src/org/lvypas/Circle.java
@@ -1,8 +1,8 @@
 package org.lvypas;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
 
 public class Circle implements Shape {
     
@@ -12,8 +12,7 @@ public class Circle implements Shape {
         return center;
     }
     
-    @Autowired
-    @Qualifier("circleRelated")
+    @Resource
     public void setCenter(Point center) {
         this.center = center;
     }
@@ -22,6 +21,16 @@ public class Circle implements Shape {
     public void draw() {
         System.out.println("Drawing Circle");
         System.out.println("Circle: Point is: (" + center.getX() + ", " + center.getY() + ")");
+    }
+    
+    @PostConstruct
+    public void initializeCircle() {
+        System.out.println("Init of Circle");
+    }
+    
+    @PreDestroy
+    public void destroyCircle() {
+        System.out.println("Destroy of Circle");
     }
 
 }
--- src/org/lvypas/DrawingApp.java
+++ src/org/lvypas/DrawingApp.java
@@ -1,14 +1,15 @@
 package org.lvypas;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext(
-                "spring.xml");        
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext(
+                "spring.xml");
+        context.registerShutdownHook();
         Shape shape = (Shape) context.getBean("circle");
         shape.draw();
 
