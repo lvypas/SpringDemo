fa28b7629b28e3e45392734725ccc5ea88c0fa59
lvypas <lyubic@gmail.com>
2013-08-29 13:52:10 +0300

    Tutorial 15 - Writing a BeanPostProcessor.

---
+++ src/org/lvypas/DisplayNameBeanPostProcessor.java
@@ -0,0 +1,22 @@
+package org.lvypas;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class DisplayNameBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName)
+            throws BeansException {
+        System.out.println("In after initialization method. Bean name is " +beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName)
+            throws BeansException {
+        System.out.println("In before initialization method. Bean name is " +beanName);
+        return bean;
+    }
+
+}
--- src/org/lvypas/Triangle.java
+++ src/org/lvypas/Triangle.java
@@ -1,9 +1,6 @@
 package org.lvypas;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class Triangle implements InitializingBean, DisposableBean{
+public class Triangle {
 
     private Point pointA;
     private Point pointB;
@@ -43,24 +40,5 @@ public class Triangle implements InitializingBean, DisposableBean{
                 + getPointC().getY() + ")");
 
     }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("Initialization init method called for Triangle");
-        
-    }
-
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("DisposableBean destroy method called for Triangle");
-        
-    }
-    
-    public void myInit() {
-        System.out.println("My init method called for Triangle");
-    }
-    
-    public void cleanUp() {
-        System.out.println("My cleanup method called for Triangle");
-    }
+   
 }
